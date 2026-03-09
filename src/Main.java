63b96480b15e083c5bce3b099e580a627312febd
Jonathan Pidono <jmathewpidono@student.ciputra.ac.id>
2026-03-09 16:08:23 +0800

    Gabungkan menjadi 1 file

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,86 @@ public class Main {
 
         double totalPayroll = 0;
 
-        for(int i = 0; i < employees.length; i++){
+        for (int i = 0; i < employees.length; i++) {
             employees[i].printInfo();
             totalPayroll += employees[i].calculateSalary();
         }
-        
+
         System.out.printf("Total Payroll: %.1f%n", totalPayroll);
+    }
+}
+
+class Employee {
+    String name;
+    double baseSalary;
+
+    Employee(String name, double baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    double calculateSalary() {
+        return baseSalary;
+    }
+
+    String getRole() {
+        return "Employee";
+    }
+
+    void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Role: " + getRole());
+        System.out.println("Salary: " + calculateSalary());
+    }
+}
+
+class Manager extends Employee {
+
+    Manager(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    double calculateSalary() {
+        return baseSalary + 2000000;
+    }
+
+    @Override
+    String getRole() {
+        return "Manager";
+    }
+}
+
+class Programmer extends Employee {
+
+    Programmer(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    double calculateSalary() {
+        return baseSalary + 1000000;
+    }
+
+    @Override
+    String getRole() {
+        return "Programmer";
+    }
+}
+
+class Intern extends Employee {
+
+    Intern(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    double calculateSalary() {
+        return baseSalary + 300000;
+    }
+
+    @Override
+    String getRole() {
+        return "Intern";
     }
 }
